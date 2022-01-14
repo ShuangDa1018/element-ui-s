5a7adaa1ce4e41e8823baf15c0db0895b1f4fe8c
ShuangDa1018 <763943829@qq.com>
2022-01-14 18:41:15 +0800

    copy components

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -34,7 +34,7 @@ module.exports = {
           {title:'dialogdrag 弹框拖拽',path: 'dialogdrag',},
           {title:'fullscreen 全屏',path: 'fullscreen',},
           // {title:'highlight-code 代码高亮',path: 'highlight-code',},
-          {title:'html-pdf 导出PDF',path: 'html-pdf',},
+          // {title:'html-pdf 导出PDF',path: 'html-pdf',},
           {title:'magnifie 放大镜',path: 'magnifie',},
           {title:'nprogress 进度条',path: 'nprogress',},
           {title:'keyboard 键盘',path: 'keyboard',},
